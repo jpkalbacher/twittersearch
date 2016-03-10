cac89b1223d682836b461c04e75552cbe5024272
Jeff Kalbacher <jpkalbacher@gmail.com>
2016-03-10 07:52:02 -0800

    edit twitter initializer

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,6 @@
 $twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['twitter_consumer_key']
   config.consumer_secret = ENV['twitter_consumer_secret']
+  config.access_token = ENV['access_token']
+  config.access_token_secret = ENV['access_token_secret']
 end
